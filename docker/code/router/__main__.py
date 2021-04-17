5e3d56e5edef7c1856d3011cf61e0745a0e40602
Fly me to the moon <git@bigly.dog>
2021-04-17 15:44:45 -0700

    ok

--- docker/code/router/__main__.py
+++ docker/code/router/__main__.py
@@ -6,6 +6,17 @@ from .consts import RUN, TEMPLATES
 from .template import j2_build, j2_render
 
 
+def _template() -> None:
+    env = {}
+    j2 = j2_build(TEMPLATES)
+    for path in walk(TEMPLATES):
+        tpl = path.relative_to(TEMPLATES)
+        dest = RUN / tpl
+        text = j2_render(j2, path=tpl, env=env)
+        dest.parent.mkdir(parents=True, exist_ok=True)
+        dest.write_text(text)
+
+
 def _parse_args() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument("op", choices=("template",))
@@ -16,15 +27,7 @@ def main() -> None:
     args = _parse_args()
 
     if args.op == "template":
-        env = {}
-
-        j2 = j2_build(TEMPLATES)
-        for path in walk(TEMPLATES):
-            tpl = path.relative_to(TEMPLATES)
-            dest = RUN / tpl
-            text = j2_render(j2, path=tpl, env=env)
-            dest.parent.mkdir(parents=True, exist_ok=True)
-            dest.write_text(text)
+        _template()
 
 
 main()
