9988829e15e2e65095435196d06097728ca3821f
Fly me to the moon <git@bigly.dog>
2021-04-17 18:09:38 -0700

    ok

--- docker/code/router/subnets.py
+++ docker/code/router/subnets.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
 from ipaddress import IPv4Network, IPv6Network
 from json import loads
-from typing import Optional
+from typing import Iterable, Iterator, Optional
 
+from std2.ipaddress import RFC_1918
 from std2.pickle import decode
 from std2.pickle.coders import ipv4_network_decoder, ipv6_network_decoder
 
 from .consts import LFS, NETWORKS
-from .ip.v4 import pick_private
 from .types import DualStack, Networks
 
 
@@ -35,22 +35,39 @@ def load_networks() -> Networks:
     return networks
 
 
+def _private_subnets(prefix: int) -> Iterator[IPv4Network]:
+    for network in RFC_1918:
+        try:
+            yield from network.subnets(new_prefix=prefix)
+        except ValueError:
+            pass
+
+
+def _pick_private(
+    existing: Iterable[IPv4Network], prefix: int
+) -> Iterator[IPv4Network]:
+    seen = {*existing}
+
+    for candidate in _private_subnets(prefix):
+        for network in seen:
+            if not candidate.overlaps(network) and not network.overlaps(candidate):
+                seen.add(candidate)
+                yield candidate
+                break
+
+
 def _v4(exclusions: str) -> _V4Stack:
     nono = map(IPv4Network, exclusions.split(LFS))
-    lan, wg, tor, guest = pick_private(nono, prefix=24)
+    lan, wg, tor, guest = _pick_private(nono, prefix=24)
     stack = _V4Stack(lan=lan, wg=wg, tor=tor, guest=guest)
     return stack
 
 
 def _v6(ula_prefix: Optional[str], ula_subnet: Optional[str]) -> _V6Stack:
-    v6_prefix = int(ula_prefix.replace(":", ""), 16)
-    v6_subnet = int(ula_subnet.replace(":", ""), 16)
-    if not v6_prefix < 2 ** 40:
-        raise ValueError()
-    elif not v6_subnet < 2 ** 16:
-        raise ValueError()
+    prefix = ula_prefix or ""
+    subnet = ula_subnet or ""
 
-    lan_v6 = f"fd00:{ula_prefix}:{ula_subnet}/64"
+    org = f"fd00:{prefix}::/80"
 
 
 def calculate_networks() -> Networks:
