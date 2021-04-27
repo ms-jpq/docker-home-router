e089845a28c733179c53a858c029b1bcc789737e
Fly me to the moon <git@bigly.dog>
2021-04-27 00:19:00 -0700

    ok

--- docker/etc/cont-init.d/04-nftables.sh
+++ docker/etc/cont-init.d/04-nftables.sh
@@ -4,4 +4,4 @@ set -eu
 set -o pipefail
 
 
-exec nft --file '/data/nftables/0-include.conf'
+exec /srv/run/nftables/nft.sh
---
+++ docker/srv/templates/nftables/nft.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+
+SCRIPT="$(
+cat << EOF
+flush ruleset
+
+include "./docker/*.conf";
+include "./user/*.conf";
+EOF
+)"
+
+
+exec nftables --file - <<< "$SCRIPT"
