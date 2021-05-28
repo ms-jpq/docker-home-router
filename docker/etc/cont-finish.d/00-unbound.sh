c544c8e0d85a6b02a0eda905343503725c0d5b73
Fly me to the moon <git@bigly.dog>
2021-05-28 11:39:39 -0700

    ok

--- docker/etc/cont-finish.d/00-unbound.sh
+++ docker/etc/cont-finish.d/00-unbound.sh
@@ -5,8 +5,8 @@ set -o pipefail
 
 
 CACHE='/data/unbound-cache.txt'
-TMP="$(mktemp)"
+TMP="$(s6-setuidgid "$USER" mktemp)"
 
 
 s6-setuidgid "$USER" /srv/run/unbound/ctl.sh dump_cache > "$TMP"
-s6-setuidgid "$USER" mv -- "$TMP" "$CACHE"
+exec s6-setuidgid "$USER" mv -- "$TMP" "$CACHE"
