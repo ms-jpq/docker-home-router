138b91cf7c4a204f6b2b3b95cbdd58f975f68cd1
Fly me to the moon <git@bigly.dog>
2021-11-29 10:20:57 -0800

    ok

--- docker/code/router/__main__.py
+++ docker/code/router/__main__.py
@@ -3,8 +3,8 @@ from typing import Sequence, Tuple
 
 from .cache.main import main as cache_main
 from .cake.main import main as cake_main
+from .domains.main import main as domains_main
 from .ifup.main import main as ifup_main
-from .lan.main import main as lan_main
 from .stats.main import main as stats_main
 from .template.main import main as template_main
 from .wireguard.main import main as wg_main
@@ -18,7 +18,7 @@ def _parse_args() -> Tuple[Namespace, Sequence[str]]:
             "cache",
             "cake",
             "ifup",
-            "lan",
+            "domains",
             "stats",
             "template",
             "wg",
@@ -36,8 +36,8 @@ def main() -> None:
         cache_main()
     elif args.op == "cake":
         cake_main()
-    elif args.op == "lan":
-        lan_main(argv)
+    elif args.op == "domains":
+        domains_main(argv)
     elif args.op == "stats":
         stats_main()
     elif args.op == "template":
