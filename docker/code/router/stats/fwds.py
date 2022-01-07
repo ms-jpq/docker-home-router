f59c4de6cbb0df393df065581725917311cf6e9f
IIIIII <github@bigly.dog>
2022-01-07 02:17:27 -0800

    ok

--- docker/code/router/stats/fwds.py
+++ docker/code/router/stats/fwds.py
@@ -1,6 +1,6 @@
 from json import dumps
 from subprocess import check_output
-from typing import AbstractSet, Tuple
+from typing import Sequence, Tuple
 
 from std2.pickle.encoder import new_encoder
 
@@ -10,11 +10,12 @@ from .subnets import load_networks
 
 
 def feed() -> str:
-    coder = new_encoder[Tuple[AbstractSet[Forwarded], AbstractSet[Available]]](
-        Tuple[AbstractSet[Forwarded], AbstractSet[Available]]
+    coder = new_encoder[Tuple[Sequence[Forwarded], Sequence[Available]]](
+        Tuple[Sequence[Forwarded], Sequence[Available]]
     )
     networks = load_networks()
-    data = coder(forwarded_ports(networks))
+    f, g = forwarded_ports(networks)
+    data = coder((tuple(f), tuple(g)))
     json = dumps(data, check_circular=False, ensure_ascii=False, allow_nan=False)
     raw = check_output(("sortd", "yaml"), text=True, input=json, timeout=SHORT_DURATION)
     return raw.strip()
