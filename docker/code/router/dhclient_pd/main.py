4358b3065c82816c8b5c7fca5b799043109f1481
IIIIII <github@bigly.dog>
2022-01-30 22:59:39 -0800

    ok

--- docker/code/router/dhclient_pd/main.py
+++ docker/code/router/dhclient_pd/main.py
@@ -1,7 +1,9 @@
+from subprocess import check_call
+
 from ..ip import ipv6_enabled
 from ..options.parser import settings
 
 
 def main() -> None:
     if settings().interfaces.wan_pd_only and ipv6_enabled():
-        pass
+        check_call(("dhclient", "-d", "-6", "-P", settings().interfaces.wan))
