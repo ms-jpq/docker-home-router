790d7f9976cbca2b668df5e36e93516d87e6afed
IIIIII <github@bigly.dog>
2022-01-30 23:02:49 -0800

    ok

--- docker/code/router/__main__.py
+++ docker/code/router/__main__.py
@@ -3,6 +3,8 @@ from typing import Sequence, Tuple
 
 from .cache.main import main as cache_main
 from .cake.main import main as cake_main
+from .dhclient_pd.main import main as dhclient_pd_main
+from .dhclient_script.main import main as dhclient_script
 from .domains.main import main as domains_main
 from .ifup.main import main as ifup_main
 from .stats.main import main as stats_main
@@ -17,8 +19,10 @@ def _parse_args() -> Tuple[Namespace, Sequence[str]]:
         choices=(
             "cache",
             "cake",
-            "ifup",
+            "dh_client",
+            "dh_script",
             "domains",
+            "ifup",
             "stats",
             "template",
             "wg",
@@ -36,6 +40,10 @@ def main() -> None:
         cache_main()
     elif args.op == "cake":
         cake_main()
+    elif args.op == "dh_client":
+        dhclient_pd_main()
+    elif args.op == "dh_script":
+        dhclient_script()
     elif args.op == "domains":
         domains_main(argv)
     elif args.op == "stats":
