6dfaa6b706f20693d1e8981e7f4d0f69158b0a2a
IIIIII <github@bigly.dog>
2022-01-30 23:07:47 -0800

    ok

--- docker/code/router/dhclient_pd/main.py
+++ docker/code/router/dhclient_pd/main.py
@@ -1,14 +1,27 @@
 from subprocess import check_call
 from time import sleep
 
-from ..consts import SHORT_DURATION
+from ..consts import DHCP_CLIENT_LEASES, RUN, SHORT_DURATION
 from ..ip import ipv6_enabled
 from ..options.parser import settings
 
 
 def main() -> None:
     if settings().interfaces.wan_pd_only and ipv6_enabled():
-        check_call(("dhclient", "-d", "-6", "-P", settings().interfaces.wan))
+        check_call(
+            (
+                "dhclient",
+                "-d",
+                "--no-pid",
+                "-lf",
+                DHCP_CLIENT_LEASES,
+                "-sf",
+                RUN / "dhclient/ctl.sh",
+                "-6",
+                "-P",
+                settings().interfaces.wan,
+            )
+        )
     else:
         while True:
             sleep(SHORT_DURATION)
