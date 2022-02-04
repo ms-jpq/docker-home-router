c47d67f7758d5c450bdf19901c3bc895c1023cc2
IIIIII <github@bigly.dog>
2022-02-04 03:49:01 -0800

    dumb ass

--- docker/code/router/dhclient/main.py
+++ docker/code/router/dhclient/main.py
@@ -1,14 +1,15 @@
+from shutil import which
 from subprocess import check_call
 from time import sleep
 
-from ..consts import DHCP_CLIENT_LEASES, RUN, SHORT_DURATION
+from ..consts import DHCP_CLIENT_LEASES, SHORT_DURATION
 from ..ip import ipv6_enabled
 from ..options.parser import settings
 
 
 def main() -> None:
     DHCP_CLIENT_LEASES.parent.mkdir(parents=True, exist_ok=True)
-    if settings().interfaces.wan_pd_only and ipv6_enabled():
+    if (tru := which("true")) and settings().interfaces.wan_pd_only and ipv6_enabled():
         check_call(
             (
                 "dhclient",
@@ -17,7 +18,7 @@ def main() -> None:
                 "-lf",
                 DHCP_CLIENT_LEASES,
                 "-sf",
-                RUN / "dhclient/ctl.sh",
+                tru,
                 "-6",
                 "-P",
                 settings().interfaces.wan,
--- docker/srv/templates/dhclient/ctl.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-set -o pipefail
-
-
-exec /venv/bin/python3 -m router ifup "$@"
