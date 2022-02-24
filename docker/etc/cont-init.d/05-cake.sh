6c1507a8550602555f9558cdd0190576e1f7fe59
IIIIII <github@bigly.dog>
2022-02-24 10:01:30 -0800

    ok

--- docker/etc/cont-init.d/05-cake.sh
+++ docker/etc/cont-init.d/05-cake.sh
@@ -4,4 +4,5 @@ set -eu
 set -o pipefail
 
 
+export PATH="/usr/sbin/bin:$PATH"
 exec /venv/bin/python3 -m router cake
--- docker/etc/cont-init.d/06-nftables.sh
+++ docker/etc/cont-init.d/06-nftables.sh
@@ -4,5 +4,5 @@ set -eu
 set -o pipefail
 
 
-export PATH="/usr/local/bin:$PATH"
+export PATH="/usr/sbin/bin:$PATH"
 exec /srv/run/nftables/nft.sh
