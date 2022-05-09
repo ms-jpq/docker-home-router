0d7711aa3bc7c58800bf14af0228a61d49d69fa4
IIIIII <github@bigly.dog>
2022-05-09 09:02:10 -0700

    update for ubuntu 22 hosts

--- docker/srv/templates/nftables/nft.sh
+++ docker/srv/templates/nftables/nft.sh
@@ -8,15 +8,5 @@ export PATH="/usr/sbin:$PATH"
 cd "$(dirname "$0")" || exit 1
 
 
-SCRIPT="$(
-cat << EOF
-flush ruleset
-
-include "./sys/*.conf";
-include "./user/*.conf";
-EOF
-)"
-
-
-nft --file - <<< "$SCRIPT"
+nft --file - < ./0-flush.conf
 printf '%s\n' 'NFT SUCC'
