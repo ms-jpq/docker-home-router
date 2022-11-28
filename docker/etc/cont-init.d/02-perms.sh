5498f8bfd5baa077d2105f47a916b65281feb269
IIIIII <git@bigly.dog>
2022-11-28 02:58:23 -0800

    ugh

--- docker/etc/cont-init.d/02-perms.sh
+++ docker/etc/cont-init.d/02-perms.sh
@@ -12,13 +12,24 @@ ARGS=(
   )
 
 
-RULES=(/config/nftables/*)
-DEST=/srv/run/nftables/user/
 
-if (( ${#RULES[@]} ))
+NFT_DEST=/srv/run/nftables/user/
+NTP_DEST=/srv/run/chrony/sources.d/
+
+mkdir -p -- "$NFT_DEST" "$NTP_DEST"
+
+
+NFTS=(/config/nftables/*)
+NTPS=(/config/ntpsources/*)
+
+if (( ${#NFTS[@]} ))
 then
-  mkdir -p -- "$DEST"
-  cp -r -- "${RULES[@]}" "$DEST"
+  cp -r -- "${NFTS[@]}" "$NFT_DEST"
+fi
+
+if (( ${#NTPS[@]} ))
+then
+  cp -r -- "${NTPS[@]}" "$NTP_DEST"
 fi
 
 exec chown -R "$USER:$USER" -- "${ARGS[@]}"
