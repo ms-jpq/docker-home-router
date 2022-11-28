275c0f4438f56a899a0592037a34868a8e900e0a
IIIIII <git@bigly.dog>
2022-11-27 22:53:11 -0800

    feeds

--- docker/code/router/stats/chrony.py
+++ docker/code/router/stats/chrony.py
@@ -1,8 +1,13 @@
 from os import linesep
 from subprocess import check_output
+from typing import Iterator
+
+
+def _feeds() -> Iterator[str]:
+    yield check_output(("chronyc", "sources"), text=True)
+    yield check_output(("chronyc", "tracking"), text=True)
+    yield check_output(("chronyc", "serverstats"), text=True)
 
 
 def feed() -> str:
-    raw_1 = check_output(("chronyc", "tracking"), text=True)
-    raw_2 = check_output(("chronyc", "serverstats"), text=True)
-    return f"{raw_1}{linesep * 3}{raw_2}"
+    return (linesep * 3).join(_feeds())
