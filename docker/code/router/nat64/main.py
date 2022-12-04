b61f94d17ed258762ff449dc65f670a656c90a50
IIIIII <git@bigly.dog>
2022-12-04 11:13:59 -0800

    reeeeeeeeeeeeeeeeeeee

--- docker/code/router/nat64/main.py
+++ docker/code/router/nat64/main.py
@@ -1,4 +1,4 @@
-from subprocess import CalledProcessError, check_call, run
+from subprocess import check_call, run
 
 from ..consts import RUN, TUNNABLE
 from ..options.parser import settings
@@ -9,25 +9,14 @@ def main() -> None:
     if TUNNABLE:
         if_name = settings().interfaces.nat64_if
         networks = load_networks()
+        ipv6 = str(next(networks.nat64.v6.hosts()))
 
         run(("ip", "link", "del", "dev", if_name))
         check_call(("tayga", "--config", RUN / "tayga" / "0-main.conf", "--mktun"))
 
         check_call(("ip", "link", "set", "dev", if_name, "up"))
+
+        check_call(("ip", "addr", "add", ipv6, "dev", if_name))
+
         check_call(("ip", "route", "add", str(networks.nat64.v4), "dev", if_name))
-        try:
-            check_call(
-                (
-                    "ip",
-                    "-6",
-                    "route",
-                    "add",
-                    str(networks.nat64.v6),
-                    "via",
-                    str(next(networks.nat64.v6.hosts())),
-                    "dev",
-                    if_name,
-                )
-            )
-        except CalledProcessError as e:
-            print(e, flush=True)
+        check_call(("ip", "-6", "route", "add", str(networks.nat64.v6), "dev", if_name))
