b683c59c9e80c02a667b7b327f6c37d83da687ef
IIIIII <git@bigly.dog>
2022-12-15 08:16:36 -0800

    tc

--- docker/code/router/stats/tc.py
+++ docker/code/router/stats/tc.py
@@ -1,9 +1,30 @@
 from os import linesep
+from re import compile
 from subprocess import check_output
+from typing import Iterator
+
+from std2.locale import si_prefixed
 
 from ..cake.main import TC_IFB
 from ..consts import SHORT_DURATION
 from ..options.parser import settings
+
+_RE_ROW = compile(r"^(?P<header>\s+\w+)(?P<cols>(?:\s+\d+)+)$")
+_RE_COLS = compile(r"\s+(?P<quantity>\d+)")
+
+
+def _parse(raw: str) -> Iterator[str]:
+    for line in raw.splitlines():
+        if m := _RE_ROW.fullmatch(line):
+            yield m.group("header")
+            for col in _RE_COLS.finditer(m.group("cols")):
+                quantity = int(col.group("quantity"))
+                parsed = si_prefixed(quantity)
+                yield parsed.rjust(len(col.group()))
+        else:
+            yield line
+
+        yield linesep
 
 
 def _feed(if_name: str) -> str:
@@ -12,7 +33,8 @@ def _feed(if_name: str) -> str:
         text=True,
         timeout=SHORT_DURATION,
     )
-    return raw.strip()
+
+    return "".join(_parse(raw))
 
 
 def feed() -> str:
