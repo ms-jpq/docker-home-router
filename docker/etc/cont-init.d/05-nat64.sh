8abedd1aadb3c89dee29339392a17c2ca1777021
IIIIII <git@bigly.dog>
2023-01-01 14:54:59 -0800

    more

--- docker/etc/cont-init.d/05-nat64.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -Eeu
-set -o pipefail
-export PATH="/usr/sbin:$PATH"
-
-
-exec -- /venv/bin/python3 -m router nat64
--- docker/etc/cont-init.d/08-nftables.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -Eeu
-set -o pipefail
-export PATH="/usr/sbin:$PATH"
-
-
-exec -- /srv/run/nftables/nft.sh
