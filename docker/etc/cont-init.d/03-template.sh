6579cea5f1769a9d1198981117e028fa2dd967a6
IIIIII <git@bigly.dog>
2023-01-01 14:38:54 -0800

    bound

--- docker/etc/cont-init.d/03-template.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -Eeu
-set -o pipefail
-export PATH="/usr/local/bin:$PATH"
-
-
-s6-setuidgid "$USER" /venv/bin/python3 -m router template
-exec -- chown -R -- root:root /srv/run/sudo
